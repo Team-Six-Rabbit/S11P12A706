a50ef09447e32d7483cc06a3a9158bce46dc4160
김현지 <chik1y@naver.com>
2024-07-18 16:48:52 +0900

    Feat: 라이브 게시판 헤더 구현 - 게시판 제목 - 제목 뒤 이미지 - 카테고리 탭

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "@/App";
+import LiveBoardPage from "@/pages/LiveBoardPage";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
+	},
+	{
+		path: "/live-board",
+		element: <LiveBoardPage />,
 	},
 ]);
 
