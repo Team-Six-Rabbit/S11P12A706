f2673a394e15afd3aeed86ef4d19c42118cdd995
5ooyeon <melthetensai@gmail.com>
2024-07-19 13:09:47 +0900

    [Feat] add regist balance game
    - add method: regist balance game(skeleton)

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/domain/User.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/domain/User.java
@@ -5,10 +5,12 @@ import java.time.LocalDate;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@ToString
 public class User {
 
     @Id
