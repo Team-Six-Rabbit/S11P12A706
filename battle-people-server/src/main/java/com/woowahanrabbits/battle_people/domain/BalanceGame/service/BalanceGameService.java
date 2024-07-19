56fb93d2139453f4fee9a771588722a69362c353
5ooyeon <melthetensai@gmail.com>
2024-07-19 14:56:38 +0900

    [Feat] Add get balance game list

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/BalanceGame/service/BalanceGameService.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/BalanceGame/service/BalanceGameService.java
@@ -1,9 +1,13 @@
 package com.woowahanrabbits.battle_people.domain.BalanceGame.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.woowahanrabbits.battle_people.domain.battle.dto.BattleReturnDto;
 
 public interface BalanceGameService {
 	void addBalanceGame(BattleReturnDto battleReturnDto);
+
+	Page<?> getBalanceGameByConditions(int category, int status, Pageable pageable);
 }
