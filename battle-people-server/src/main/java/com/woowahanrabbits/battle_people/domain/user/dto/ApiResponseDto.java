6a153d6b05fcfc5f72c934844530d64ad2876709
Yangseyeon <seyeon0207@gmail.com>
2024-07-22 15:48:03 +0900

    [Feat] openVidu setting docker

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/dto/ApiResponseDto.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/dto/ApiResponseDto.java
@@ -1,13 +1,8 @@
 package com.woowahanrabbits.battle_people.domain.user.dto;
 
-import com.woowahanrabbits.battle_people.domain.battle.dto.BattleBoardResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.domain.Page;
-
-import java.util.List;
-
 
 @Getter
 @Setter
