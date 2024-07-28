d955124b7d6e32ed57f5ab00559ba0331728fe98
Yangseyeon <seyeon0207@gmail.com>
2024-07-28 22:40:12 +0900

    [Feat] Live List Detail Info

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/vote/infrastructure/VoteOpinionRepository.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/vote/infrastructure/VoteOpinionRepository.java
@@ -3,7 +3,6 @@ package com.woowahanrabbits.battle_people.domain.vote.infrastructure;
 import com.woowahanrabbits.battle_people.domain.vote.domain.VoteOpinion;
 import com.woowahanrabbits.battle_people.domain.vote.domain.VoteOpinionId;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
